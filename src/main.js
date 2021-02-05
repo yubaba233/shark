9c3eee93bb2089082623ec434e9d25268564bdbf
yubaba233 <2460250637@qq.com>
2021-02-05 16:42:33 +0800

    init

--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import router from './router'
 import './plugins/element.js'
 import store from './store'
 import moment from 'moment'
-import echarts from 'echarts'
+// import echarts from 'echarts'
 
 Vue.config.productionTip = false
 Vue.prototype.$moment = moment
-Vue.prototype.$echarts = echarts
+// Vue.prototype.$echarts = echarts
 
 new Vue({
   router,
